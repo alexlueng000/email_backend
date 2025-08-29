e9ea456bde4fb9f30259c6358a8f4feabb9abb41
AlexLueng <alexlueng000@gmail.com>
2025-08-29 15:18:31 +0800

    fix bugs

--- app/utils.py
+++ app/utils.py
@@ -141,14 +141,16 @@ def update_project_info_company_D(contract_number: str, company_d_name: str) -> 
         "appType": "APP_R55Z1QDKMB0VILUQRNJA",             # 固定为 APP（宜搭应用）
         "systemToken": "6Q866L81UPU4TY6NBOTYZBB28OUC3K1N7EM9LG91",  # 宜搭 System Token
         "formUuid": "FORM-TP866D91MJO5MFN08AMJGA8H52ZV3HPX6XRALD1",
+        "useLatestVersion": True,
         "dataCreateFrom": 0,          # 可选：0=全部；1=我创建；2=我参与
         "userId": "571848422",           # 这里换成有权限访问该宜搭应用/表单的用户
         "searchCondition": json.dumps(search_conditions, ensure_ascii=False),
-        "formDataJson": json.dumps(formDataJson, ensure_ascii=False)
+        "formDataJson": json.dumps(formDataJson, ensure_ascii=False),
+        "formInstanceId": "FINST-AU966L916HAXS1196SGGK8K56HGO3S2SDWXDMKU9"
      }
 
     try:
-        resp = requests.post("https://api.dingtalk.com/v2.0/yida/forms/instances/insertOrUpdate", headers=headers, data=json.dumps(body))
+        resp = requests.put("https://api.dingtalk.com/v2.0/yida/forms/instances", headers=headers, data=json.dumps(body))
         logger.info("✅ 回写项目信息表单，D公司更新成功，ID：%s", resp.json()["result"])
         print("✅ 回写项目信息表单，D公司更新成功，ID：%s", resp.json()["result"])
         return "更新表单成功"
